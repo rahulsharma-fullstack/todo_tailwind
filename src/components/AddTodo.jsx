888ea6f5ef3f6215fbc8198643a1fcb1304044f0
kin6Man <domin12313xyz@gmail.com>
2024-09-10 12:17:33 -0400

    basic setup for tailwind and todo app

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AddTodo = () => {
+  return (
+    <>
+      <div className="data-add flex-row space-x-4 ">
+        <input type="text"></input>
+        <input type="date"></input>
+        <button> add</button>
+      </div>
+    </>
+  );
+};
+
+export default AddTodo;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div>
+      <ol>
+        <li>
+          <p>gym</p> <p>21/09/2013</p>
+          <button>X</button>
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+export default TodoList;
