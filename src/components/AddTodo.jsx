51b2ab39f8381e0a2c6721c84521a8369cedea0c
kin6Man <domin12313xyz@gmail.com>
2024-09-14 12:55:03 -0400

    optimising code using useRef and form validation

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,40 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const AddTodo = ({handleItemList}) => {
-  const [itemName, setItemName] = useState('')
-  const [itemDate, setItemDate] = useState('')
+  // const [itemName, setItemName] = useState('')
+  // const [itemDate, setItemDate] = useState('')
 
+  const itemNameInput = useRef('');
+  const itemNameDate = useRef('');
   
-  const handleTextItems = (e) =>{
-    setItemName(e.target.value)
-  }
-  const handleDateItems = (e) =>{
-    setItemDate(e.target.value)
-  }
-  const handleAddButton = () => {
-
-    handleItemList(itemName, itemDate)
-    setItemName("")
-    setItemDate("")
+  // const handleTextItems = (e) =>{
+  //   setItemName(e.target.value)
+  // }
+  // const handleDateItems = (e) =>{
+  //   setItemDate(e.target.value)
+  // }
+  const handleAddButton = (e) => {
+    e.preventDefault();
+    handleItemList(itemNameInput.current.value, itemNameDate.current.value)
+    // setItemName("")
+    // setItemDate("")
   }
   
   return (
     <>
-      <div className="data-add flex flex-row space-x-4 place-content-center mb-8">
+      <form className="data-add flex flex-row space-x-4 place-content-center mb-8" onSubmit={handleAddButton}>
         <input
           type="text"
           className="todo_input border-gray-300 drop-shadow-xl rounded-lg border-2 outline-none text-center"
-          placeholder="Enter Todo" onChange={handleTextItems} value={itemName}
+          placeholder="Enter Todo" ref={itemNameInput} value={itemNameInput.current.value}
         ></input>
         <input
           type="date"
-          className="todo_input_date border-gray-300 drop-shadow-xl rounded-lg border-2 outline-none text-center" onChange={handleDateItems} value={itemDate}
+          className="todo_input_date border-gray-300 drop-shadow-xl rounded-lg border-2 outline-none text-center" ref={itemNameDate} value={itemNameDate.current.value}
         ></input>
-        <button className="add_button mx-3 border-2 rounded-2xl px-4 py-1 font-medium bg-sky-700 shadow-xl text-white hover:bg-slate-700" onClick={handleAddButton}>
+        <button className="add_button mx-3 border-2 rounded-2xl px-4 py-1 font-medium bg-sky-700 shadow-xl text-white hover:bg-slate-700">
           {" "}
           Add
         </button>
-      </div>
+      </form>
     </>
   );
 };
